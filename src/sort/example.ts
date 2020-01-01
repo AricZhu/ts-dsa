c325db2d2da5c91bed35395341c61936388b009e
aric.zhu <2594609013@qq.com>
2020-01-01 11:27:46 +0800

    new: add heap sort

--- src/sort/example.ts
+++ src/sort/example.ts
@@ -1,9 +1,21 @@
-import { insertSort, shellSort } from './index'
+import { insertSort, shellSort, insertHeapEle, heapSort } from './index'
 
+console.log('========== 插入排序测试 ==========')
 let arr1 = [34, 8, 64, 51, 32, 21]
 console.log(`data is: ${arr1}`)
 console.log(`after insertSort, data: ${insertSort(arr1, arr1.length)}`)
 
+console.log('========== 希尔排序测试 ==========')
 let arr2 = [81, 94, 11, 96, 12, 35, 17, 95, 28, 58, 41, 75, 15]
 console.log(`data is: ${arr2}`)
 console.log(`after shellSort, data: ${shellSort(arr2, arr2.length)}`)
+
+console.log('========== 堆排序测试 ==========')
+let heapElm = [31, 41, 59, 26, 53, 58, 97]
+console.log(`data is: ${heapElm}`)
+let arr3: number[] = []
+heapElm.forEach(el => insertHeapEle(arr3, el))
+console.log(`after insert, the heap is: ${arr3}`)
+console.log(`after heapSort, data: ${heapSort(arr3, heapElm.length)}`)
+
+console.log('========== end ==========')
--- src/sort/index.ts
+++ src/sort/index.ts
@@ -31,3 +31,55 @@ export function shellSort (arr: number[], n: number): number[] {
     }
     return arr
 }
+
+// 堆排序
+// 插入一个元素并保持堆序
+export function insertHeapEle (arr: number[], val: number): void {
+    if (arr.length === 0) {
+        arr[1] = val
+        return
+    }
+    let iPos = arr.length
+    let pPos = Math.floor(iPos / 2)
+    while (pPos > 0 && val > arr[pPos]) {
+        arr[iPos] = arr[pPos]
+        iPos = pPos
+        pPos = Math.floor(iPos / 2)
+    }
+    arr[iPos] = val
+}
+
+// 堆排序
+export function heapSort (arr: number[], n: number): number[] {
+    let size = n
+    while (size >= 1) {
+        [arr[1], arr[size]] = [arr[size], arr[1]]
+        size--
+        filterDown(arr, size)
+    }
+
+    return arr
+}
+
+// 调整堆的根元素，使其保持堆序
+function filterDown (arr: number[], size: number): void {
+    let [iPos, val] = [1, arr[1]]
+    while (iPos * 2 + 1 <= size) {
+        let [leftChild, rightChild] = [iPos * 2, iPos * 2 + 1]
+        if (val > arr[leftChild] && val > arr[rightChild]) {
+            break
+        } else if (arr[leftChild] > arr[rightChild]) {
+            arr[iPos] = arr[leftChild]
+            iPos = leftChild
+        } else {
+            arr[iPos] = arr[rightChild]
+            iPos = rightChild
+        }
+    }
+    if (iPos * 2 + 1 <= size || iPos * 2 > size || val > arr[iPos * 2]) {
+        arr[iPos] = val
+    } else {
+        arr[iPos] = arr[2 * iPos]
+        arr[2 * iPos] = val
+    }
+}
